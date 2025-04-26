1ff3beea7eab530a6f452f451ad2657dc3e27d0c
BahaaEddin AlAila <bahaelaila7@gmail.com>
2025-04-26 15:19:24 +0000

    add cpo trainer

---
+++ run_smol_cpo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export PYTHONPATH="$PYTHONPATH:alignment-handbook/src/"
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/multi_gpu.yaml alignment-handbook/scripts/run_cpo.py recipes/smollm2/cpo/config_smol.yaml
