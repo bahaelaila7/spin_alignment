da2b5276324ef76acb0d931ce34000076cee2382
BahaaEddin AlAila <bahaelaila7@gmail.com>
2025-04-30 16:02:42 +0000

    reflect trl import changes

--- run_smol_dpo.sh
+++ run_smol_dpo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export PYTHONPATH="$PYTHONPATH:alignment-handbook/src/"
+export PYTHONPATH="$PYTHONPATH:alignment-handbook/src/:trl/"
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True ACCELERATE_LOG_LEVEL=info accelerate launch  --main_process_port 29509 --config_file recipes/accelerate_configs/multi_gpu.yaml alignment-handbook/scripts/run_dpo.py recipes/smollm2/dpo/config_smol.yaml
--- run_smol_sft.sh
+++ run_smol_sft.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export PYTHONPATH="$PYTHONPATH:alignment-handbook/src/"
+export PYTHONPATH="$PYTHONPATH:alignment-handbook/src/:trl/"
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/multi_gpu.yaml alignment-handbook/scripts/run_sft.py recipes/smollm2/sft/config_smol.yaml
